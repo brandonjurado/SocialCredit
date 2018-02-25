3611883cc3b2a5320fc2f924244c52b933b01c84
Michael Osei <mosei27@live.com>
2018-02-24 19:23:23 -0600

    Update frontend ui with bootstrap

---
+++ app/static/js/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+ var webpack = require('webpack');
+
+ module.exports = {
+    watch: true,
+     entry: './app_main.js',
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'main.bundle.js'
+     },
+     module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['es2015']
+                 }
+             }
+         ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
